68741d055f3220473f9daf9f8e39b770e16be73d
norhan <norhankhidr12@gmail.com>
2026-01-20 21:52:44 +0200

    Add files via upload

---
+++ MPU_test/MPU_test.ino
@@ -0,0 +1,28 @@
+#include "MPU.h"
+#include <Wire.h>
+
+
+MPU6050 mpu6050(Wire);
+
+int pwmValue = 0;
+
+void setup() {
+
+  Serial.begin(115200);
+  Wire.begin();
+  mpu6050.begin();
+  mpu6050.calcGyroOffsets(true);
+
+}
+
+void loop() {
+  mpu6050.update();
+  Serial.print("angleX : ");
+  Serial.print(mpu6050.getAngleX());
+  Serial.print(",");
+  Serial.print(mpu6050.getAngleY());
+  Serial.print(",");
+  Serial.println(mpu6050.getAngleZ());
+
+  delay(500);
+}
